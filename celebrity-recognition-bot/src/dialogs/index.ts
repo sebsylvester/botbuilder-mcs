4e98e4b546725b42a15e458950febbf514c73494
Sebastian <sebastian@furonto.com>
2017-01-19 14:31:34 +0100

    file cleanup

--- celebrity-recognition-bot/src/dialogs/index.ts
+++
@@ -1,5 +0,0 @@
-// Re-export the dialogs so that they can be used with a single import statement
-export { root } from './root';
-export { help } from './root';
-export { newRequest } from './newRequest';
-export { suggestions } from './suggestions';
