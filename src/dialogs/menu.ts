b676a7f9fa62a5c33e808967528e46659ce80868
Sebastian <sebastian@furonto.com>
2017-01-21 22:32:19 +0100

    Remove default index value, fix comment

--- src/dialogs/menu.ts
+++ src/dialogs/menu.ts
@@ -1,14 +1,13 @@
 import { IDialogResult, ListStyle, Prompts, Session } from 'botbuilder';
 import { APIs, CognitiveServices } from '../helpers/consts';
 
-// The /firstRun dialog handler
+// The /menu dialog handler
 export const menu = [
     (session: Session, args: Object, next: Function) => {
         session.send('I currently know 2 different APIs.');
         session.send('With Computer Vision, I can recognize celebrities.');
         session.send('With Emotion, I can recognize facial expressions.');
 
-        // Use Computer Vision as default
         const { selectedAPI } = session.userData;
         const message = `I'm currently using the ${APIs[selectedAPI].name} API.`;
         const choices = APIs.map(item => item.name);
@@ -17,8 +16,7 @@ export const menu = [
         Prompts.choice(session, message, choices, options);
     },
     (session: Session, results: IDialogResult<any>) => {
-        // Use Computer Vision as default
-        const { index = CognitiveServices.ComputerVision } = results.response || {};
+        const { index } = results.response;
         session.userData.selectedAPI = index;
 
         session.endDialog("OK, from now on I'll be using the %s API.", APIs[index].name);
